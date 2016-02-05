642ef83d600aa2b512c8f394c3beecdc952eefbb
gencebay <gencebay.demir@gmail.com>
2016-02-06 00:19:20 +0200

    Extensions

---
+++ src/Cloud.Common/Core/RandomMessageGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Cloud.Common.Contracts;
+using System.Collections.Generic;
+
+namespace Cloud.Common.Core
+{
+    public static class RandomMessageGenerator
+    {
+        public static List<MessageDefinitions> Messages = new List<MessageDefinitions>
+        {
+            new MessageDefinitions
+            {
+                Commands = new[] { CommandType.Build },
+                Owner = "Server1",
+                Recipient = new Recipient
+                {
+                    AssemblyName = "Sample.ConsoleApp",
+                    Version = "1.0.0-*"
+                }
+            },
+
+            new MessageDefinitions
+            {
+                Owner = "Server1",
+                Recipient = new Recipient
+                {
+                    AssemblyName = "Sample.AnotherApp",
+                    Version = "1.0.0-*"
+                }
+            },
+        };
+    }
+}
---
+++ src/Cloud.Server/Extensions/BuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Cloud.Server.Middleware;
+using Microsoft.AspNet.Builder;
+
+namespace Cloud.Server.Extensions
+{
+    public static class BuilderExtensions
+    {
+        public static void UseSocket(this IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<SocketRequestMiddleware>();
+        }
+    }
+}

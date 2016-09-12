593ce972581c5bf5da7cd188c4a29c40c74fe2ac
Gencebay <gencebay.demir@gmail.com>
2016-09-12 09:35:44 +0300

    xunit and moq dependencies

--- ToBeCompiled/SampleApp.Test/SampleTest.cs
+++ ToBeCompiled/SampleApp.Test/SampleTest.cs
@@ -2,20 +2,12 @@ using Xunit;
 
 namespace SampleApp.Tests
 {
-    // see example explanation on xUnit.net website:
-    // https://xunit.github.io/docs/getting-started-dnx.html
     public class SampleTest
     {
         [Fact]
         public void PassingTest()
         {
             Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
         }
 
         int Add(int x, int y)
